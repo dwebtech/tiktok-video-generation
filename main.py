e1dc22246ac4279a13155aa18cd9236394f63527
Katie Burgess <70536041+burgess01@users.noreply.github.com>
2024-04-05 14:58:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import services.scripting_service as scripting_service
 import services.voice_generator_service as voice_generator_service
+import services.video_editing_service as video_editing_service
 
 def main():
     print("Welcome to Tik-Tok Travel Video Generator")
@@ -8,6 +9,10 @@ def main():
     
     message_content = scripting_service.get_travel_information(location)
     voice_content = voice_generator_service.synthesize_speech(message_content)
+
+    # need finished audio and video files
+    # edited_video = video_editing_service.run(audio, video)
+    print("Finished Editing your Tik-Tok Travel Video!")
 
 
 
